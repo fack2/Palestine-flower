422830ffeb970d859f6cd69bf6359a25d675ec17
yaqootturman <yaqoot.turman@gmail.com>
2019-08-07 16:32:47 +0300

    partials and css styling

--- src/app.js
+++ src/app.js
@@ -5,14 +5,19 @@ const app = express();
 const router = require('./controllers/index')
 const helpers = require('./views/helpers/index');
 
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.use(router)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+
 app.engine(
     'hbs',
     exphbs({
         extname: 'hbs',
         layoutsDir: path.join(__dirname, 'views', 'layouts'),
+        partialsDir: path.join(__dirname, 'views', 'partials'),
         defaultLayout: 'main',
         helpers: helpers
 
